d665dcd4601a249afdf737b052063a1bebf2dcda
Quek Xin Quan <s10243101@connect.np.edu.sg>
2023-04-30 02:23:16 +0800

    Prac 2

---
+++ app/src/main/java/com/example/week2prac/MainActivity.java
@@ -0,0 +1,85 @@
+package com.example.week2prac;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.res.Configuration;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    final String TITLE = "Main Activity";
+    User user1;
+    Button follow;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.v(TITLE, "On Create:");
+        user1 = new User("user1","hello",001,false);
+        follow =findViewById(R.id.button);
+        if (user1.Followed)
+        {
+            follow.setText("Unfollow");
+        }
+        else
+        {
+            follow.setText("Follow");
+        }
+
+    }
+    @Override
+    protected void onStart(){
+        super.onStart();
+        Log.v(TITLE, "On Start:");
+    }
+    @Override
+    protected void onResume(){
+        super.onResume();
+        Log.v(TITLE, "On Resume:");
+        follow.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                if (user1.Followed)
+                {
+                    user1.Followed = false;
+                    follow.setText("Follow");
+                    Log.v(TITLE,"follow");
+
+                }
+                else {
+                    user1.Followed = true;
+                    follow.setText("Unfollow");
+                    Log.v(TITLE,"Unfollow");
+                }
+
+
+            }
+
+        }
+        );
+
+    }
+    protected void onPause(){
+
+        super.onPause();
+        Log.v(TITLE, "On Pause:");
+    }
+    @Override
+    protected void onStop(){
+        super.onStop();
+        Log.v(TITLE, "On Stop:");
+    }
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        Log.v(TITLE, "On Destroy:");
+    }
+
+}
